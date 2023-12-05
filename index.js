a09365b2c79f0c0968e5a3eeec764d4705b054ac
MScott-Dev <MScott0199@gmail.com>
2023-12-05 14:15:50 -0500

    Added badges for different languages

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateReadME = ({ title, summary, imageURL, description, usage, depndencies, liveLink, installing, executing, authors }) => 
+
+const htmlbadge = (pL) => {
+    const htmlImage = "https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white";
+
+    if (pL.includes("HTML")) {
+        return `<img src="${htmlImage} " />`
+    } else {
+        return ''
+    }
+}
+
+const cssbadge = (pL) => {
+    const cssImage = "https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white";
+
+    if (pL.includes("CSS")) {
+        return `<img src="${cssImage} " />`;
+    } else {
+        return ''
+    }
+}
+
+const jsbadge = (pL) => {
+    const jsImage = "https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E";
+
+    if (pL.includes("JavaScript")) {
+        return `<img src="${jsImage} " />`;
+    } else {
+        return ''
+    }
+}
+
+const generateReadME = ({ title, pL, summary, imageURL, description, usage, dependencies, liveLink, installing, executing, authors }) => 
   `# ${title}
+  ${htmlbadge(pL)} ${cssbadge(pL)} ${jsbadge(pL)} 
+  <br>
 ${summary}
 
 ## Description 
@@ -18,7 +51,7 @@ ${usage}
 
 ## Dependencies
 
-${depndencies}
+${dependencies}
 3. <a href="${liveLink}" alt="WeatherApp">A link to the webpage</a>
 
 ## Installing
@@ -42,6 +75,12 @@ inquirer
       message: 'What is the title of your project?',
     },
     {
+      type: 'checkbox',
+      message: 'What languages did this project include?',
+      name: 'pL',
+      choices: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
       type: 'input',
       name: 'summary',
       message: 'Can you give me a small summary of your project?',
@@ -60,6 +99,11 @@ inquirer
       type: 'input',
       name: 'usage',
       message: 'What is the usage of your project?',
+    },
+    {
+      type: 'input',
+      name: 'dependencies',
+      message: 'What is are the dependencies of your project?',
     },
     {
       type: 'input',
@@ -88,4 +132,5 @@ inquirer
     fs.writeFile('README.md', readmePageContent, (err) =>
       err ? console.log(err) : console.log('Successfully created README.md!')
     );
+    
   });
